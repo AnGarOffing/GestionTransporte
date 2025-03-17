2addfab98bfb8933e4a63b6cb6c6915d11e35c3c
AndresAyure <andresgarcia.ag.x@gmail.com>
2025-03-17 11:53:25 +0100

    Se crea el proyecto junto con el gitignore indicando los archivos a excluir

---
+++ GestionTransporte/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GestionTransporte.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
